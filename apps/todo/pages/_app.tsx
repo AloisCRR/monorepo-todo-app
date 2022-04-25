8038c77c9d19403271bd00a6b747a3d71f7d9377
Alois Carrera <aloiscarrera@gmail.com>
2022-04-24 22:01:36 -0500

    refactor: 💄 add base header styles
    Also add MantineProvider

--- apps/todo/pages/_app.tsx
+++ apps/todo/pages/_app.tsx
@@ -1,22 +1,32 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { AppShell, Footer, Header, Title } from '@mantine/core';
+import { AppShell, MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import ToDoAppHeader from '../components/header/header';
 import './styles.css';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <AppShell
-      header={
-        <Header height={60}>
-          <Title align="center" order={3}>
-            ToDo App
-          </Title>
-        </Header>
-      }
-      footer={<Footer height={60}>Footer</Footer>}
-    >
-      <Component {...pageProps} />
-    </AppShell>
+    <>
+      <Head>
+        <title>ToDo App</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark'
+        }}
+      >
+        <AppShell header={<ToDoAppHeader />}>
+          <Component {...pageProps} />
+        </AppShell>
+      </MantineProvider>
+    </>
   );
 }
 
